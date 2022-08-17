c9c42e02fb9868fff71c15b96941e27fd6a1474a
Homiak332 <Stasmaksimov2003@gmail.com>
2022-08-17 10:25:16 +0700

    делаем шапку

--- cyber-zen-labs/src/pages/Home.jsx
+++ cyber-zen-labs/src/pages/Home.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import {
+  DivMainHeaderSC,
+  DivMainSC,
+  DivMainBorderSC,
+  DivFlexHeaderSC
+} from '../styled-components-css/styles.navbar';
 const Home = () => {
   return (
-    <div>Home</div>
+    <DivMainSC>
+      <DivMainBorderSC>
+
+      <DivMainHeaderSC>
+        <DivFlexHeaderSC>
+
+        </DivFlexHeaderSC>
+      </DivMainHeaderSC>
+      </DivMainBorderSC>
+      
+      </DivMainSC>
   )
 }
 
---
+++ cyber-zen-labs/src/styled-components-css/styles.navbar.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components/macro';
+
+import { Link } from 'react-router-dom';
+export const DivMainSC = styled.div`
+width: 100%;
+display: grid;
+`;
+export const DivMainHeaderSC = styled.div`
+    width:1720px;
+`;
+
+export const DivMainLogoSC = styled.div`
+
+`;
+
+export const DivMainBorderSC = styled.div`
+
+  display: grid;
+  justify-self: center;
+  max-width: 1720px;
+  width: 100%;
+`;
+export const DivFlexHeaderSC = styled.div`
+
+
+`;
