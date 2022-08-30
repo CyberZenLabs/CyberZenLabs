fe1bfd6843d3b27f2e216b22e4ebbaba005981a2
RuslanGordin <101972460+ruslanGordin@users.noreply.github.com>
2022-08-30 17:17:50 +0700

    адаптив страницы leadership

--- cyber-zen-labs/src/components/NavBarTop.jsx
+++ cyber-zen-labs/src/components/NavBarTop.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import GlobalDispatchContext from "../global_dispatch_context";
 import {
   DivContainerNavBarSC,
@@ -12,53 +12,51 @@ import {
   DivShowReelCenterSC,
   DivContainerNavBarHomeSC,
   LinkToBurgerSC,
-  IconBurgerSC
+  IconBurgerSC,
 } from "../styled-components-css/styled-navbar";
 
-
 const NavBarTop = () => {
-  const {state, dispatch} = useContext(GlobalDispatchContext);
-  const {
-    isBlackBack, isHome
-  } = state
+  const { state, dispatch } = useContext(GlobalDispatchContext);
+  const { isBlackBack, isHome } = state;
   return (
     <>
-    {
-        isHome === true ?
+      {isHome === true ? (
         <DivMaxWidthSC>
-        <DivContainerNavBarHomeSC>
-          <LinkToHomeSC to='/'>
-            <DivLogoBoxSC>
-              <LogoNavBarSC />
-              <TextNavBarSC>Cyberzen Labs</TextNavBarSC>
-            </DivLogoBoxSC>
-          </LinkToHomeSC>
+          <DivContainerNavBarHomeSC>
+            <LinkToHomeSC to="/">
+              <DivLogoBoxSC>
+                <LogoNavBarSC />
+                <TextNavBarSC>Cyberzen Labs</TextNavBarSC>
+              </DivLogoBoxSC>
+            </LinkToHomeSC>
 
-          <DivShowReelSC>
-            <LinkShowReelSC to="#"  isBlackBack={isBlackBack}>SHOWREEL</LinkShowReelSC>
-          </DivShowReelSC>
-        </DivContainerNavBarHomeSC>
-      </DivMaxWidthSC>
-
-
-      :<DivMaxWidthSC>
-        <DivContainerNavBarSC>
-          <LinkToHomeSC to='/'>
-            <DivLogoBoxSC>
-              <LogoNavBarSC />
-              <TextNavBarSC>Cyberzen Labs</TextNavBarSC>
-            </DivLogoBoxSC>
-          </LinkToHomeSC>
-          <DivShowReelCenterSC>
-            <LinkShowReelSC to="#"  isBlackBack={isBlackBack}>SHOWREEL</LinkShowReelSC>
-          </DivShowReelCenterSC>
-          <LinkToBurgerSC to ="/menu">
-            <IconBurgerSC></IconBurgerSC>
-          </LinkToBurgerSC>
-        </DivContainerNavBarSC>
-      </DivMaxWidthSC>
-      }
-      
+            <DivShowReelSC>
+              <LinkShowReelSC to="#" isBlackBack={isBlackBack}>
+                SHOWREEL
+              </LinkShowReelSC>
+            </DivShowReelSC>
+          </DivContainerNavBarHomeSC>
+        </DivMaxWidthSC>
+      ) : (
+        <DivMaxWidthSC>
+          <DivContainerNavBarSC>
+            <LinkToHomeSC to="/">
+              <DivLogoBoxSC>
+                <LogoNavBarSC />
+                <TextNavBarSC>Cyberzen Labs</TextNavBarSC>
+              </DivLogoBoxSC>
+            </LinkToHomeSC>
+            <DivShowReelCenterSC>
+              <LinkShowReelSC to="#" isBlackBack={isBlackBack}>
+                SHOWREEL
+              </LinkShowReelSC>
+            </DivShowReelCenterSC>
+            <LinkToBurgerSC to="/menu">
+              <IconBurgerSC></IconBurgerSC>
+            </LinkToBurgerSC>
+          </DivContainerNavBarSC>
+        </DivMaxWidthSC>
+      )}
     </>
   );
 };
