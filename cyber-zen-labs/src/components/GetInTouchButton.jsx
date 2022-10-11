9fb7b00dcf4670c5113ab317c57eb2a36edb3f1d
ilya <moroz2002ilya@gmail.com>
2022-10-11 13:31:15 +0700

    new button

--- cyber-zen-labs/src/components/GetInTouchButton.jsx
+++ cyber-zen-labs/src/components/GetInTouchButton.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import '../styles/git-button.css'
+import React, { useEffect, useState } from "react";
+import "../styles/git-button.css";
+import { Stage, Layer, Rect, Text, Circle, Line } from "react-konva";
+import Konva from "konva";
+import { DivGitContainerSC } from "../styled-components-css/styled-git-button";
 
 const GetInTouchButton = () => {
+  const [width, setWidth] = useState(null);
+  const [height, setHeight] = useState(null);
+
+  useEffect(() => {
+    var offsetHeight = document.getElementById("gitButton").offsetHeight;
+    var offsetWidth = document.getElementById("gitButton").offsetWidth;
+    // console.log("ffff", offsetHeight, offsetWidth);
+    setWidth(offsetWidth);
+    setHeight(offsetHeight);
+  }, [window.innerWidth]);
+
   return (
     <>
-        <div className='container-git'>
+      <DivGitContainerSC id="gitButton">
+        <Stage width={width} height={height}>
+          <Layer>
+            <Circle x={width / 2} y={width / 2} radius={50} fill="green" />
+          </Layer>
+        </Stage>
+      </DivGitContainerSC>
+      {/* <div className='container-git'>
             <div className='outer-circle'></div>
-        </div>
+        </div> */}
     </>
-  )
-}
+  );
+};
 
-export default GetInTouchButton
+export default GetInTouchButton;
--- cyber-zen-labs/src/styled-components-css/styled-git-button.jsx
+++ cyber-zen-labs/src/styled-components-css/styled-git-button.jsx
@@ -1,1 +1,10 @@
 import styled from "styled-components/macro";
+
+export const DivGitContainerSC = styled.div`
+  width: 100%;
+  height: 100%;
+  display: grid;
+  justify-items: center;
+  align-items: center;
+  border: 1px solid white;
+`;
